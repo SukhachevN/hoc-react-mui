b8e9ea4146ae7a7b2f05278d9e2d0a0208f2a98f
Nikita Sukhachev <traed.traed@mail.ru>
2022-07-27 22:46:45 +0300

    lesson-8

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { MuiTypography } from './components/MuiTypography';
+import { MuiButton } from './components/MuiButton';
+import { MuiTextField } from './components/MuiTextField';
+
+import './App.css';
+import { MuiSelect } from './components/MuiSelect';
+
+const App = () => {
+  return (
+    <div className='App'>
+      {/* <MuiTypography /> */}
+      {/* <MuiButton /> */}
+      {/* <MuiTextField /> */}
+      <MuiSelect />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,34 @@
+import { Typography } from '@mui/material';
+
+const MuiTypography = () => (
+  <div>
+    <Typography variant='h1'>h1 heading</Typography>
+    <Typography variant='h2'>h2 heading</Typography>
+    <Typography variant='h2'>h3 heading</Typography>
+    <Typography variant='h4' component='h1' gutterBottom>
+      h4 heading
+    </Typography>
+    <Typography variant='h5'>h5 heading</Typography>
+    <Typography variant='h6'>h6 heading</Typography>
+    <Typography variant='subtitle1'>subtitle 1</Typography>
+    <Typography variant='subtitle2'>subtitle 2</Typography>
+    <Typography variant='body1'>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi
+      consectetur ex quis! Eligendi, repellendus consequuntur consectetur eos
+      illum minima reprehenderit laborum veniam eius alias neque, fuga quis ut,
+      assumenda vitae iure minus aut. Ab, sed cupiditate, ea atque itaque
+      molestias voluptates eos voluptate eligendi, ipsum ducimus. Reiciendis
+      blanditiis voluptas magni.
+    </Typography>
+    <Typography variant='body2'>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum maiores,
+      adipisci, incidunt facilis explicabo similique magnam fugiat, culpa
+      repudiandae temporibus cupiditate accusantium aspernatur provident
+      quibusdam consequatur quos. Laudantium quod veniam debitis optio, et ipsum
+      reiciendis dolore a dignissimos minus deleniti iste quibusdam itaque ab
+      sint!
+    </Typography>
+  </div>
+);
+
+export { MuiTypography };
